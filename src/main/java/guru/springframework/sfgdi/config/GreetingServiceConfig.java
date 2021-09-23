fac084a76ee8b8d7b290a787d18afc676e206f71
sergeyshevchenko <serhii.shevchenko.privatbank.ua>
2021-09-23 19:20:38 +0300

    version 1.0.5
    add properties file;
    add value annotation.

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -3,15 +3,31 @@ package guru.springframework.sfgdi.config;
 import com.springframework.pets.DogPetService;
 import com.springframework.pets.PetFactory;
 import com.springframework.pets.PetService;
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:datasource.properties")
 @Configuration
 public class GreetingServiceConfig {
+
+  @Bean
+  FakeDataSource fakeDataSource(@Value("${some.username}") String username,
+                                @Value("${some.password}") String password,
+                                @Value("${some.jdbcurl}") String jdbcurl) {
+    FakeDataSource fakeDataSource = new FakeDataSource();
+    fakeDataSource.setUsername(username);
+    fakeDataSource.setPassword(password);
+    fakeDataSource.setJdbcurl(jdbcurl);
+
+    return fakeDataSource;
+  }
 
   @Bean
   PetFactory petFactory() {
