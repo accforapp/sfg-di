ce912f19eb71a4d91fd12db8234166c476959f6d
sergeyshevchenko <serhii.shevchenko.privatbank.ua>
2021-09-28 19:13:34 +0300

    version 1.0.6
    add profiles application.properties

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
-@PropertySource("classpath:datasource.properties")
 @Configuration
 public class GreetingServiceConfig {
 
